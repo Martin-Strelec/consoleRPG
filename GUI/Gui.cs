47d5e6186ea2dd54760ea72c1e25cbb1dfd6a749
Martin-Strelec <m.strelec2021@gmail.com>
2024-06-08 19:59:18 +0100

    Added Error & Progress Bar

--- GUI/Gui.cs
+++ GUI/Gui.cs
@@ -4,32 +4,7 @@ namespace consoleRPG
 {
     class Gui
     {
-        public static void Title(string str)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            str = $"==== {str} ====\n\n";
-
-            Console.Write(str);
-            Console.ResetColor();
-        }
-
-        public static void MenuTitle(string str)
-        {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            str = $"=== {str}\n";
-
-            Console.Write(str);
-            Console.ResetColor();
-        }
-
-        public static void MenuOption(params string[] str)
-        {
-            for (int i = 1; i <= str.Length; i++)
-            {
-                Console.Write($"- ({i}): {str[i-1]}\n");
-            }
-        }
-
+        //Input methods
         public static void GetInput(string str)
         {
             str = $"- {str}: ";
@@ -55,14 +30,71 @@ namespace consoleRPG
             }
             return input.Value;
         }
-
-        public static void Announcment(string str)
+        
+        //Announcments
+        public static void Title(string str)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            str = $"\n(~) {str}!\n";
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            str = $"==== {str} ====\n\n";
 
             Console.Write(str);
             Console.ResetColor();
+        }
+
+        public static void MenuTitle(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            str = $"=== {str}\n";
+
+            Console.Write(str);
+            Console.ResetColor();
+        }
+
+        public static void MenuOption(params string[] str)
+        {
+            for (int i = 1; i <= str.Length; i++)
+            {
+                Console.Write($"- ({i}): {str[i-1]}\n");
+            }
+        }
+        
+        public static void Announcment(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            str = $"\n(~) {str}\n";
+
+            Console.Write(str);
+            Console.ResetColor();
+        }
+        
+        public static void Error(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            str = $"(!) {str}\n";
+
+            Console.Write(str);
+            Console.ResetColor();
+        }
+        
+        //Visual guides
+        public static string ProgressBar(int min, int max, int width)
+        {
+            double percentage = (double) min / max;
+            int done = Convert.ToInt32(percentage * width);
+            int undone = width - done;
+
+            string bar = "[";
+            for (int i = 0; i < done; i++)
+            {
+                bar += "*";
+            }
+            for (int i = 0; i < undone; i++)
+            {
+                bar += "-";
+            }
+            bar += "]";
+
+            return bar;
         }
 
     }
